d5db38bbafe016bfa0ccc629c00f90730f04ba4c
m0rp3x <yurash612@gmail.com>
2022-09-22 17:57:43 +0300

    В далекой далекой поджелудочной

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,81 @@
+﻿
+
+namespace ConsoleApp8;
+
+internal class aboba
+{
+
+
+    public static void Main(string[] args)
+    {
+
+        Cat aboba = new Cat();
+        Cat chikibombony = new Cat();
+        Cat lololowka = new Cat();
+        Cat liciy = new Cat();
+        Cat shaturnaya = new Cat();
+        aboba.name = "Абоба";
+        chikibombony.name = "Чикибомбони";
+        lololowka.name = "Лололовка";
+        liciy.name = "Лысый";
+        shaturnaya.name = "Шатурная";
+        while (true)
+        {
+
+
+
+            
+
+            Console.WriteLine("Выберете пункт меню:");
+            Console.WriteLine("1.Показать всех кошечек которые у вас есть");
+            Console.WriteLine("2.Покормить кошечек");
+            Console.WriteLine("3.Повести кошечек в туалет");
+            {
+
+
+                char ch = char.Parse(Console.ReadLine() ?? string.Empty);
+                {
+
+
+                    switch (ch)
+                    {
+                        case '1':
+                        {
+                            aboba.Print();
+                            chikibombony.Print();
+                            lololowka.Print();
+                            liciy.Print();
+                            shaturnaya.Print();
+
+
+                            break;
+                        }
+                        case '2':
+                        {
+                            aboba.Eat();
+                            chikibombony.Eat();
+                            lololowka.Eat();
+                            liciy.Eat();
+                            shaturnaya.Eat();
+
+                            break;
+                        }
+                        case '3':
+                        {
+                          aboba.pee();
+                          chikibombony.pee();
+                          lololowka.pee();
+                          liciy.Eat();
+                          shaturnaya.pee();
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+            
+            
+            
--- ConsoleApp8/cat.cs
+++ ConsoleApp8/cat.cs
@@ -1,40 +1,49 @@
 
+namespace ConsoleApp8;
 
-namespace ConsoleApp7;
-
-
-
-public class cat
+public class Cat
 {
     public string name;
-    public int ves;
+    public int ves = 20;
     public int hp = 100;
-    public int kishka = 0;
-
-
-
+    public int kishka;
+    public const int eyes = 2;
+    public const int min_ves = 20;
+    public const int max_ves = 150;
+    public int summ_eda;
+    
     public void Print()
     {
         Console.WriteLine($"Кличка:{name},Вес:{ves},Здоровье:{hp}");
     }
-
-    public void eat()
+    public void Eat()
     {
-        if (kishka < 5) 
+        if (kishka != 5) 
         {
+            summ_eda += 20;
+            kishka+=1;
+            ves += 20;
             Console.WriteLine($"Вы покормили {name}, его вес теперь {ves}");
-            kishka += 1;
+            Console.WriteLine($"сумма всего съеденного корма {summ_eda}");
         }
         else
         {
-            Console.WriteLine($"Вы перекормили {name}, она умерла");
-
+            Console.WriteLine($"Вы перекормили {name}, он бахнул");
         }
-    }
 
+    }
     public void pee()
     {
-        
+        if (kishka >= 2)
+        {
+            kishka=0;
+            ves -= 10;
+            Console.WriteLine($"Котик {name} cходил в туалет,его вес {ves}");
+        }
+        else
+        {
+                Console.WriteLine("Ещё не время отправлять кошек в туалет");
+        }
     }
 }
 
