596f2f1c910779d70fcddca1d23588799979e509
m0rp3x <yurash612@gmail.com>
2022-09-28 08:48:56 +0300

    К-К-К-К-ОМБО

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,6 +1,10 @@
-﻿
+﻿using System.Data.SqlClient;
+using System;
+using System.Data;
+using System.Configuration;
 
 namespace ConsoleApp8;
+
 
 internal class aboba
 {
@@ -8,28 +12,30 @@ internal class aboba
 
     public static void Main(string[] args)
     {
-
-        Cat aboba = new Cat();
-        Cat chikibombony = new Cat();
-        Cat lololowka = new Cat();
-        Cat liciy = new Cat();
-        Cat shaturnaya = new Cat();
-        aboba.name = "Абоба";
-        chikibombony.name = "Чикибомбони";
-        lololowka.name = "Лололовка";
-        liciy.name = "Лысый";
-        shaturnaya.name = "Шатурная";
+        
+        
+        Console.WriteLine("Добро пожаловать в nyan_cat_wars, в этой игре вы сможете играть за котиков,они едят,спят,скребут стены и прочее ");
+        Console.Write("Введите имя первого котика:");
+        Cat aboba = Cat.Create($"{Console.ReadLine()}", 100);
+        Console.Write("Введите имя Второго котика:");
+        Cat chikibombony = Cat.Create($"{Console.ReadLine().ToString()}", 100);
+        Console.Write("Введите имя Третьего котика:");
+        Cat lololowka = Cat.Create($"{Console.ReadLine().ToString()}", 100);
+        Console.Write("Введите имя Четвертого котика:");
+        Cat liciy = Cat.Create($"{Console.ReadLine().ToString()}", 100);
+        Console.Write("Введите имя Пятого котика:");
+        Cat shaturnaya = Cat.Create($"{Console.ReadLine().ToString()}", 100);
+        
         while (true)
         {
-
-
-
-            
-
             Console.WriteLine("Выберете пункт меню:");
             Console.WriteLine("1.Показать всех кошечек которые у вас есть");
             Console.WriteLine("2.Покормить кошечек");
             Console.WriteLine("3.Повести кошечек в туалет");
+            Console.WriteLine("4.Число ваших кошек");
+            Console.WriteLine("5.Посмотреть статусы ваших котяток");
+            Console.WriteLine("6.Сохранить прогресс");
+            
             {
 
 
@@ -62,12 +68,32 @@ internal class aboba
                         }
                         case '3':
                         {
-                          aboba.pee();
-                          chikibombony.pee();
-                          lololowka.pee();
-                          liciy.Eat();
-                          shaturnaya.pee();
+                            aboba.pee();
+                            chikibombony.pee();
+                            lololowka.pee();
+                            liciy.Eat();
+                            shaturnaya.pee();
 
+                            break;
+                        }
+                        case '4':
+                        {
+                            Cat.GetCount();
+
+                            break;
+                        }
+                        case '5':
+                        {
+                            Cat.kal();
+                            break;
+                        }
+                        case '6':
+                        {
+                            aboba.Save();
+                            chikibombony.Save();
+                            lololowka.Save();
+                            liciy.Save();
+                            shaturnaya.Save();
                             break;
                         }
                     }
@@ -76,6 +102,3 @@ internal class aboba
         }
     }
 }
-            
-            
-            
--- ConsoleApp8/cat.cs
+++ ConsoleApp8/cat.cs
@@ -1,49 +1,124 @@
-
+using System.Configuration;
+using System.Data.SqlClient;
 namespace ConsoleApp8;
+
+
+
+
 
 public class Cat
 {
-    public string name;
-    public int ves = 20;
+    public  string Name { get; set; }
+    private static int Ves { get; set; }
+    private static int ID { get; set; }
+
     public int hp = 100;
-    public int kishka;
+    public static int Kishka;
     public const int eyes = 2;
     public const int min_ves = 20;
     public const int max_ves = 150;
     public int summ_eda;
-    
+    public static string Status = "live";
+
+    public Cat(string name, int ves)
+    {
+        Name = name;
+        Ves = ves;
+        ID++;
+        if (Status == "died")
+        {
+            ID -= 1;
+        }
+    }
+
+    public static Cat Create(string name, int ves) => new Cat(name, ves);
+
     public void Print()
     {
-        Console.WriteLine($"Кличка:{name},Вес:{ves},Здоровье:{hp}");
+        Console.WriteLine($"Кличка:{Name},Вес:{Ves},Здоровье:{hp}:Статус:{Status}");
     }
+
     public void Eat()
     {
-        if (kishka != 5) 
+        if (Kishka <= 5 && Status == "live")
         {
             summ_eda += 20;
-            kishka+=1;
-            ves += 20;
-            Console.WriteLine($"Вы покормили {name}, его вес теперь {ves}");
+            Kishka += 1;
+            Ves += 20;
+            Console.WriteLine($"Вы покормили {Name}, его вес теперь {Ves}");
             Console.WriteLine($"сумма всего съеденного корма {summ_eda}");
+        }
+        else if (Kishka == 6)
+        {
+            Console.WriteLine("Котику очень плохо перестаньте его кормить");
+
+        }
+
+        if (Kishka == 7)
+        {
+            kal();
+        }
+
+        if (Status == "died")
+        {
+            Console.WriteLine("Вы не можете выполнить этого действия так как ваши котики мертвы");
+        }
+    } 
+    public void pee()
+    {
+        if (Kishka >= 2)
+        {
+            Kishka += 1;
+            Ves -= 10;
+            Console.WriteLine($"Котик {Name} cходил в туалет,его вес {Ves}");
         }
         else
         {
-            Console.WriteLine($"Вы перекормили {name}, он бахнул");
+            Console.WriteLine("Ещё не время отправлять кошек в туалет");
         }
 
     }
-    public void pee()
+
+    public void kal()
     {
-        if (kishka >= 2)
+        if (Kishka > 5)
         {
-            kishka=0;
-            ves -= 10;
-            Console.WriteLine($"Котик {name} cходил в туалет,его вес {ves}");
+            Status = "died";
+            Console.WriteLine($"Состояние котика {Name} Сейчас {Status}");
         }
         else
         {
-                Console.WriteLine("Ещё не время отправлять кошек в туалет");
+            Status = "live";
+            Console.WriteLine($"Состояние котика {Name} Сейчас {Status}");
+
+        }
+    }
+
+    public static void GetCount()
+    {
+        Console.WriteLine(ID);
+    }
+
+    
+   
+
+    public void Save()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        Console.WriteLine(connectionString);
+        var connection = new SqlConnection(connectionString);
+        connection.Open();
+        SqlCommand command;
+        string abobka = "INSERT INTO cats(id, name, ves) VALUES(@id, @name, @ves)";
+        using (command = new SqlCommand(abobka,connection ))
+        {
+            SqlParameter nameParam = new SqlParameter("@id", ID);
+            SqlParameter nameParam2 = new SqlParameter("@name", Name);
+            SqlParameter nameParam3 = new SqlParameter("@ves", Ves);
+            command.Parameters.Add(nameParam);
+            command.Parameters.Add(nameParam2);
+            command.Parameters.Add(nameParam3);
+            command.ExecuteReader();
         }
     }
 }
-
